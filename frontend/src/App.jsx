561e1e57e92e7df155ec510c9c648a7c69d3c7bc
JRfeghaly <josephfeghaly2004@gmail.com>
2026-01-07 06:23:03 -0500

    readme more readable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -448,10 +448,11 @@ const styles = {
   },
   readmeHeader: {
     fontWeight: 900,
-    fontSize: 36,   // ⬅ increase size (try 32–40)
+    fontSize: 38,
+    letterSpacing: 2,
     cursor: "pointer",
     userSelect: "none",
-    letterSpacing: 2, // ⬅ makes it feel more "TITLE"
+    color: "#000",          // ⬅ FORCE BLACK
   },  
   readmeText: {
     marginTop: 14,
